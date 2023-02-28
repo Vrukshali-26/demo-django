093aac0490f9204c81319c26ac07c6ebdce473e7
Vrukshali-26 <tvrukshali@gmail.com>
2023-02-28 11:14:26 +0530

    Added files

---
+++ sample/hello/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello World!")
+
+def greet(request, name):
+    return render(request, "hello/index.html",
+                  {
+                    "name": name.capitalize()
+                  })
